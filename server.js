e59a021ee999a8f8b1fd4e87aafd74bc410e92bc
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-09-26 14:49:47 -0400

    reupdating

--- server.js
+++ server.js
@@ -59,12 +59,11 @@ const getScrapFromBestbuy = (searchInput, res) => {
             )
           );
         });
-        res.json({ body: JSON.stringify(result) });
         return result;
       })
       .then(result =>
         db.Item.create(result).then(result =>
-          res.json({
+          res.status(200).json({
             body: `${result.length} ${
               result.length > 1 ? "items are" : "item is"
             } scraped!`
