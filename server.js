4e2e6699de8340e0b8a183592baaf119a9423201
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-09-26 20:39:32 -0400

    change some function

--- server.js
+++ server.js
@@ -24,53 +24,7 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const getScrapFromBestbuy = (searchInput, res) => {
-  let url = `https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&sc=Global&st=${searchInput}&type=page&usc=All%20Categories`;
-  try {
-    axios
-      .get(url)
-      .then(function(response) {
-        let $ = cheerio.load(response.data);
-        let result = new Array();
-        $("li.sku-item").each(function(i, element) {
-          let title = $(this)
-            .find(".sku-header")
-            .children("a")
-            .text();
-          let price = $(this)
-            .find(".priceView-hero-price.priceView-customer-price")
-            .find("span")
-            .html();
-          if (price) price = parseInt(price.replace("$<!-- -->", ""));
-          else price = " - ";
-          let imageUrl = $(this)
-            .find(".product-image")
-            .attr("src");
-          let link =
-            "https://www.bestbuy.com" +
-            $(this)
-              .find(".sku-header")
-              .children("a")
-              .attr("href");
-          result.push(
-            Object.assign(
-              {},
-              { category: searchInput, title, price, imageUrl, link }
-            )
-          );
-        });
-        res.status(200).json({
-          body: `${result.length} ${
-            result.length > 1 ? "items are" : "item is"
-          } scraped!`
-        });
-        return result;
-      })
-      .then(result => db.Item.create(result));
-  } catch (err) {
-    res.json({ body: err });
-  }
-};
+const getScrapFromBestbuy = (searchInput, res) => {};
 
 const MONGODB_URI = config.db || "mongodb://localhost/mongoHeadlines";
 
@@ -103,8 +57,52 @@ app.get("/saved", async (req, res) => {
 });
 
 app.get("/scrape", async (req, res) => {
-  await db.Item.deleteMany({ category: req.query.searchInput });
-  getScrapFromBestbuy(req.query.searchInput, res);
+  try {
+    await db.Item.deleteMany({ category: req.query.searchInput });
+    let url = `https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&sc=Global&st=${req.query.searchInput}&type=page&usc=All%20Categories`;
+    axios
+      .get(url)
+      .then(function(response) {
+        let $ = cheerio.load(response.data);
+        let result = new Array();
+        $("li.sku-item").each(function(i, element) {
+          let title = $(this)
+            .find(".sku-header")
+            .children("a")
+            .text();
+          let price = $(this)
+            .find(".priceView-hero-price.priceView-customer-price")
+            .find("span")
+            .html();
+          if (price) price = parseInt(price.replace("$<!-- -->", ""));
+          else price = " - ";
+          let imageUrl = $(this)
+            .find(".product-image")
+            .attr("src");
+          let link =
+            "https://www.bestbuy.com" +
+            $(this)
+              .find(".sku-header")
+              .children("a")
+              .attr("href");
+          result.push(
+            Object.assign(
+              {},
+              { category: req.query.searchInput, title, price, imageUrl, link }
+            )
+          );
+        });
+        res.status(200).json({
+          body: `${result.length} ${
+            result.length > 1 ? "items are" : "item is"
+          } scraped!`
+        });
+        return result;
+      })
+      .then(result => db.Item.create(result));
+  } catch (err) {
+    res.json({ body: err });
+  }
 });
 
 app.get("/items", async (req, res) => {
